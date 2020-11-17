e16636199932441b232ec19e4053be9156c4d84d
Simon Koch <simon.koch@tu-braunschweig.de>
2020-11-17 13:22:33 +0100

    nodes to represent nodes in a dotfile graph

---
+++ src/main/scala/de/halcony/creator/dotfile/Node.scala
@@ -0,0 +1,13 @@
+package de.halcony.creator.dotfile
+
+case class Node(identifier : String, attributes : (String,String)*) extends DotFileElement {
+
+  override def dotString: String = {
+    attributes match {
+      case Nil => s"$identifier;"
+      case x => s"$identifier " +  "[label=\"" + x.map(pair => s"${pair._1}=${pair._2}").mkString(",") + "\"];"
+    }
+
+  }
+
+}
