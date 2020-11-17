054598276df3e3954dc545a2739a4189e4c503df
Simon Koch <simon.koch@tu-braunschweig.de>
2020-11-17 13:24:13 +0100

    adapted DotGraph add methods to allow for chaining

--- src/main/scala/de/halcony/creator/dotfile/DotGraph.scala
+++ src/main/scala/de/halcony/creator/dotfile/DotGraph.scala
@@ -2,8 +2,8 @@ package de.halcony.creator.dotfile
 
 trait DotGraph extends DotFileElement {
 
-  def addEdge(edge : Edge)
-  def addNode(node : Node)
+  def addEdge(edge : Edge) : DotGraph
+  def addNode(node : Node) : DotGraph
 
 }
 
@@ -12,14 +12,16 @@ class DirectedGraph(name : String) extends DotGraph {
   val edges : collection.mutable.Set[Edge] = collection.mutable.Set()
   val nodes : collection.mutable.Map[String, Node] = collection.mutable.Map()
 
-  def addEdge(edge: Edge): Unit = {
+  def addEdge(edge: Edge): DotGraph = {
     assert(nodes.contains(edge.start))
     assert(nodes.contains(edge.target))
     edges.addOne(edge)
+    this
   }
 
-  override def addNode(node: Node): Unit = {
+  override def addNode(node: Node): DotGraph = {
     nodes.addOne((node.identifier, node))
+    this
   }
 
   override def dotString: String = {
