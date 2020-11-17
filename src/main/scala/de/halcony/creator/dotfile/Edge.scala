f80aefbc819a45d189f78a1d143e359940a65e70
Simon Koch <simon.koch@tu-braunschweig.de>
2020-11-17 13:22:19 +0100

    edges to represent an edge in a dot file graph, currently only directed edges supported

---
+++ src/main/scala/de/halcony/creator/dotfile/Edge.scala
@@ -0,0 +1,19 @@
+package de.halcony.creator.dotfile
+
+trait Edge extends DotFileElement {
+
+  def start : String
+  def target : String
+
+}
+
+case class DirectedEdge(start : String, target : String, attributes : (String,String)*) extends Edge {
+
+  override def dotString: String = {
+    attributes match {
+      case Nil => s"${start} -> ${target};"
+      case x => s"${start} -> ${target} " + "[label=\"" + x.map(pair => s"${pair._1}=${pair._2}").mkString(",") + "\"];"
+    }
+  }
+
+}
