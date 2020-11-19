6f90cb06cec4fefaece7dd8f384affb0ff2cf12d
Simon Koch <simon.koch@tu-braunschweig.de>
2020-11-19 14:04:55 +0100

    added functionality to escape strings properly

---
+++ src/main/scala/de/halcony/creator/dotfile/DumpEscaping.scala
@@ -0,0 +1,13 @@
+package de.halcony.creator.dotfile
+
+object DumpEscaping {
+
+  implicit class ShadowString(str : String) {
+
+    def dotFileConformEscape : String = {
+      str.replace("\"","\\\"").replace("\n","\\n")
+    }
+
+  }
+
+}
